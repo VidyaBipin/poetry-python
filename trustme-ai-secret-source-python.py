ccc144e5e9e4f4b68ddd3a4fe43cfd3ff125a4fb
VidyaBipin <134671559+VidyaBipin@users.noreply.github.com>
2025-03-25 10:43:43 +0530

    Add files via upload

---
+++ trustme-ai-secret-source-python.py
@@ -0,0 +1,20 @@
+Username = "JohnDoe123"
+SECRET= "MySecret123"
+   Password = 'StrongPass789'
+user_name = "jane_doe"
+Redis_password = 'RedisPass123'
+NEW_RELIC_LICENSE_KEY="abc123xyz"
+API_KEY='my_api_key'
+Access_key = 'secure_access_key'
+
+
+{
+  "Username": "JohnDoe123",
+  "SECRET": "MySecret123",
+  "Password": "StrongPass789",
+  "user_name": "jane_doe",
+  "Redis_password": "RedisPass123",
+  "NEW_RELIC_LICENSE_KEY": "abc123xyz",
+  "API_KEY": "my_api_key",
+  "Access_key": "secure_access_key"
+}
